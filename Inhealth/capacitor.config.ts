d5f8ed996f3b1ad975c2678ee637de403ac6b0e7
devfutsa <thabangmofokeng@futsagroup.co.za>
2021-10-06 15:01:34 +0200

    beta

---
+++ Inhealth/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Inhealth',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
